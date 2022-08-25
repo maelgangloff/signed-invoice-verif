02111a201421c3fdd59216fbe449b783f289037a
Maël Gangloff <contact@maelgangloff.fr>
2022-08-25 22:08:01 +0200

    fix: Fix black screen when reload app

--- lib/QRViewReader.dart
+++ lib/QRViewReader.dart
@@ -33,10 +33,7 @@ class QRViewReaderState extends State<QRViewReader> {
     return Scaffold(
       body: Column(
         children: <Widget>[
-          Expanded(
-            flex: 3,
-            child: _buildQrView(context)
-            ),
+          Expanded(flex: 5, child: _buildQrView(context)),
           Expanded(
             flex: 1,
             child: FittedBox(
@@ -44,7 +41,7 @@ class QRViewReaderState extends State<QRViewReader> {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
-                  const Text('Please present a digital signature stamp'),
+                  const Text('Present a digital signature stamp'),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.center,
@@ -53,28 +50,35 @@ class QRViewReaderState extends State<QRViewReader> {
                         margin: const EdgeInsets.all(8),
                         child: ElevatedButton(
                             onPressed: () async {
-                              FilePickerResult? result = await FilePicker.platform.pickFiles(
-                                allowMultiple: false,
-                                allowCompression: false,
-                                allowedExtensions: ['pem', 'pub', 'key'],
-                                dialogTitle: 'Load a public key file',
-                                type: FileType.custom,
-                                withData: true,
-                                withReadStream: false
-                              );
-                              if(result == null) return;
+                              FilePickerResult? result =
+                                  await FilePicker.platform.pickFiles(
+                                      allowMultiple: false,
+                                      allowCompression: false,
+                                      allowedExtensions: ['pem', 'pub', 'key'],
+                                      dialogTitle: 'Load a public key file',
+                                      type: FileType.custom,
+                                      withData: true,
+                                      withReadStream: false);
+                              if (result == null) return;
                               PlatformFile file = result.files.first;
                               Uint8List? data = file.bytes;
-                              if(data == null) return;
+                              if (data == null) return;
                               setState(() {
-                                publicKey = ECPublicKey(String.fromCharCodes(data));
+                                publicKey =
+                                    ECPublicKey(String.fromCharCodes(data));
                               });
                             },
                             onLongPress: (() => publicKey = null),
-                            style: ElevatedButton.styleFrom(
-                              primary: Colors.white
-                            ),
-                            child: Icon(publicKey == null ? Icons.key_off_rounded : Icons.key_rounded, color: publicKey == null ? Colors.red : Colors.green, size: 30)),
+                            style:
+                                ElevatedButton.styleFrom(primary: Colors.white),
+                            child: Icon(
+                                publicKey == null
+                                    ? Icons.key_off_rounded
+                                    : Icons.key_rounded,
+                                color: publicKey == null
+                                    ? Colors.orange
+                                    : Colors.green,
+                                size: 30)),
                       ),
                       Container(
                         margin: const EdgeInsets.all(8),
@@ -83,13 +87,17 @@ class QRViewReaderState extends State<QRViewReader> {
                               await controller?.toggleFlash();
                               setState(() {});
                             },
-                            style: ElevatedButton.styleFrom(
-                              primary: Colors.white
-                            ),
+                            style:
+                                ElevatedButton.styleFrom(primary: Colors.white),
                             child: FutureBuilder(
                               future: controller?.getFlashStatus(),
                               builder: (context, snapshot) {
-                                return Icon(snapshot.data == false ? Icons.flash_off_rounded : Icons.flash_on_rounded, color: Colors.brown, size: 30);
+                                return Icon(
+                                    snapshot.data == false
+                                        ? Icons.flash_off_rounded
+                                        : Icons.flash_on_rounded,
+                                    color: Colors.brown,
+                                    size: 30);
                               },
                             )),
                       ),
@@ -130,87 +138,90 @@ class QRViewReaderState extends State<QRViewReader> {
       controller.pauseCamera();
       try {
         ECPublicKey? key = publicKey;
-        if(key != null) {
+        if (key != null) {
           final JWT jwt = JWT.verify(scanData.code ?? '', key);
           _showInvoiceInformation(
-            context,
-            'Valid signature!',
-            jwt.header?['kid'],
-            jwt.issuer,
-            jwt.subject,
-            jwt.payload['ref'],
-            DateTime.fromMillisecondsSinceEpoch(jwt.payload['iat'] * 1000),
-            DateTime.fromMillisecondsSinceEpoch(jwt.payload['dueDate'] * 1000),
-            jwt.payload['curr'],
-            jwt.payload['amt'],
-            jwt.payload['qty'],
-            jwt.payload['line'],
-            jwt.payload['pay']);
+              context,
+              'Valid signature!',
+              jwt.header?['kid'],
+              jwt.issuer,
+              jwt.subject,
+              jwt.payload['ref'],
+              DateTime.fromMillisecondsSinceEpoch(jwt.payload['iat'] * 1000),
+              DateTime.fromMillisecondsSinceEpoch(
+                  jwt.payload['dueDate'] * 1000),
+              jwt.payload['curr'],
+              jwt.payload['amt'],
+              jwt.payload['qty'],
+              jwt.payload['line'],
+              jwt.payload['pay']);
         } else {
           final jwt = JwtDecoder.decode(scanData.code ?? '');
           _showInvoiceInformation(
-            context,
-            'Unable to authenticate',
-            'No kid',
-            jwt['iss'],
-            jwt['sub'],
-            jwt['ref'],
-            DateTime.fromMillisecondsSinceEpoch(jwt['iat'] * 1000),
-            DateTime.fromMillisecondsSinceEpoch(jwt['dueDate'] * 1000),
-            jwt['curr'],
-            jwt['amt'],
-            jwt['qty'],
-            jwt['line'],
-            jwt['pay']);
+              context,
+              'Unable to authenticate',
+              'No kid',
+              jwt['iss'],
+              jwt['sub'],
+              jwt['ref'],
+              DateTime.fromMillisecondsSinceEpoch(jwt['iat'] * 1000),
+              DateTime.fromMillisecondsSinceEpoch(jwt['dueDate'] * 1000),
+              jwt['curr'],
+              jwt['amt'],
+              jwt['qty'],
+              jwt['line'],
+              jwt['pay']);
         }
       } catch (e) {
         _showInformation(context, 'Error', e.toString());
       }
     });
+    controller.pauseCamera();
+    controller.resumeCamera();
   }
 
   void _onPermissionSet(BuildContext context, QRViewController ctrl, bool p) {
     if (!p) {
-      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Unable to scan without camera access permission')));
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text('Unable to scan without camera access permission')));
     }
   }
 
   void _showInformation(BuildContext context, String title, String content) {
     showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return AlertDialog(
-        title: Text(title),
-        content: Text(content),
-        actions: <Widget>[
-          ElevatedButton(
-            child: const Text('OK'),
-            onPressed: () {
-              controller!.resumeCamera();
-              Navigator.of(context).pop();
-            },
-          ),
-        ],
-      );
-    },
-  );
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(content),
+          actions: <Widget>[
+            ElevatedButton(
+              child: const Text('OK'),
+              onPressed: () {
+                controller!.resumeCamera();
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   void _showInvoiceInformation(
-    BuildContext context,
-    String title,
-    String? kid,
-    String? issuer,
-    String? subject,
-    String? reference,
-    DateTime? issueDate,
-    DateTime? dueDate,
-    String? currency,
-    double? amount,
-    int? quantity,
-    int? line,
-    dynamic pay
-    ) {
+      BuildContext context,
+      String title,
+      String? kid,
+      String? issuer,
+      String? subject,
+      String? reference,
+      DateTime? issueDate,
+      DateTime? dueDate,
+      String? currency,
+      double? amount,
+      int? quantity,
+      int? line,
+      dynamic pay) {
     final DateFormat dateFormatter = DateFormat('yyyy-MM-dd');
     _showInformation(context, title, '''Signed by: ${kid ?? 'no kid'}
 From : $issuer
--- lib/main.dart
+++ lib/main.dart
@@ -9,23 +9,38 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Read an invoice')),
+      appBar: AppBar(title: const Text('Verify an invoice')),
       body: Center(
-        child: Column(
-          children: <Widget>[
-            const SizedBox(height: 10),
-            const Text('Welcome to the application for authenticating invoices'),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => const QRViewReader(),
-                ));
-              },
-              child: const Text('Scan a digital signature stamp'),
+        child: Column(children: <Widget>[
+          const SizedBox(height: 10),
+          Container(
+            padding: const EdgeInsets.all(10),
+            margin: const EdgeInsets.all(5),
+            child: RichText(
+                text: const TextSpan(
+                    text:
+                        '''Welcome to the application for authenticating invoices
+
+
+The application works in two different modes:
+
+- Reading of the invoice without authentication of the cryptographic signature (default mode). This mode is active when the lower left key is orange. The application only reads the contents of the stamp without verifying the signature.
+
+- Verification of the authenticity of the invoice. This mode is active when the lower left key is green. The application will ask you to select the document issuer's public key file. When a stamp is scanned, the signature is verified. If the signature does not match, an error is thrown and the invoice is marked as not authentic.
+''',
+                    style: TextStyle(color: Colors.black, fontSize: 14.5))),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            autofocus: true,
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => const QRViewReader(),
+              ));
+            },
+            child: const Text('Scan a digital signature stamp'),
           )
-          ]
-        ),
+        ]),
       ),
     );
   }
