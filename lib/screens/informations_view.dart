81d059e26a162121c31f7ad3d9520e9f98c17d07
lolocomotive <loic.eznt@hotmail.com>
2022-08-27 16:38:48 +0200

    Improved info screen

--- lib/screens/informations_view.dart
+++ lib/screens/informations_view.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class InformationsView extends StatelessWidget {
   const InformationsView({
@@ -8,22 +10,59 @@ class InformationsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Verifying an invoice')),
+      appBar: AppBar(title: const Text('Info')),
       body: Center(
-        child: Column(children: <Widget>[
-          const SizedBox(height: 10),
-          Container(
-            padding: const EdgeInsets.all(10),
-            margin: const EdgeInsets.all(5),
-            child: RichText(
-              text: TextSpan(text: '''By default the app does not verify the signatures.
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "Using the app",
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+              ),
+              const Text('''By default the app does not verify the signatures.
 To verify a stamp, import the public key by clicking on the key icon.
-You can toggle the flash with the flash button.
-''', style: Theme.of(context).textTheme.bodyMedium),
-            ),
+You can toggle the flash with the flash button.'''),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "About the app",
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+              ),
+              RichText(
+                text: TextSpan(
+                  children: [
+                    const TextSpan(text: "This app is open source and published on "),
+                    TextSpan(
+                      text: "GitHub",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).colorScheme.primary,
+                        decoration: TextDecoration.underline,
+                      ),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          launchUrl(
+                              Uri.parse(
+                                "https://github.com/maelgangloff/signed-invoice-verif",
+                              ),
+                              mode: LaunchMode.externalApplication);
+                        },
+                    ),
+                    const TextSpan(
+                        text:
+                            ". Go there to contribute, donate, submit a feature request or a bug report? Any contributions are very welcome!")
+                  ],
+                  style: Theme.of(context).textTheme.bodyLarge,
+                ),
+              ),
+            ],
           ),
-          const SizedBox(height: 20),
-        ]),
+        ),
       ),
     );
   }
